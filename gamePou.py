7a90ba439ba6fef5bfb4987fab51ccf1e456fb5e
LuisHGonz94 <drums4gusto@gmail.com>
2023-12-14 14:16:17 +0100

    Agregacion de metodos e instancias.

--- gamePou.py
+++ gamePou.py
@@ -4,8 +4,8 @@ class Pou:
   # inciciar
   def __init__(self, name):
     self.name = name
-    self.age = 0
-    self.hunger = 0
+    self.age = random.randint(1, 10)
+    self.hunger = random.randint(1, 10)
     self.energy = 0
     self.happiness = 0
     self.health = 0
@@ -19,19 +19,31 @@ class Pou:
     print("Happiness:", self.happiness)
     print("Health:", self.health)
 
-  def __str__(self):
+  def __str__(self): #__str__ seria la alternativa (lo mismo) a __repr__
     return f"Name: {self.name}\nAge: {self.age}\nHunger: {self.hunger}\nEnergy: {self.energy}\nHappiness: {self.happiness}\nHealth: {self.health}"
 
   def play(self):
-    # self.hunger += 1
-    # self.energy += 1
-    # self.happiness += 1
-    # self.health += 1
+    self.hunger += 1
+    self.energy += 1
+    self.happiness += 1
+    self.health += 1
 
-    def eat(self):
-        pass
+    # Add code
+    if(self.energy <= 0):
+      self.happiness -= 1
+    else:
+      self.happiness += 1
+
+def eat(self):
+    self.hunger -= 1
+    self.happiness += 1
+    #pass #to indicate that the body of the class 'eat' was intentionally left blank.
 
   # add more methods
+def sleep(self):
+  self.energy += 5
+  self.happiness += 1
+  self.hunger += 3
 
 toto = Pou("Toto")
 
@@ -40,6 +52,14 @@ while True:
 
   if option == "play":
     toto.play()
-  # add more methods
+    toto.status()
+# add more methods
+  elif option == "eat":
+    toto.eat()
+    toto.status()
+  elif option == "sleep":
+    toto.sleep()
+    toto.status()
   else:
     break
+   
